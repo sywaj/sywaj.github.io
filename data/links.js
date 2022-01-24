896ba2a4d45798068c1f8a801ec97e3c85af4094
syws <88713229+sywaj@users.noreply.github.com>
2022-01-24 23:34:29 +0800

    Update links.js

--- data/links.js
+++ data/links.js
@@ -1,1 +1,139 @@
+//常用网站
+let often  =[
+    {url:"https://baidu.com/",name:"百度"},
+    {url:"https://bilibili.com/",name:"哔哩哔哩"},
+    {url:"https://pan.baidu.com/",name:"百度网盘"},
+    {url:"https://www.aliyundrive.com/",name:"阿里云盘"},
+    {url:"https://fanyi.baidu.com",name:"百度翻译"},
+    {url:"https://www.yinxiang.com/",name:"印象笔记"},
+    {url:"http://i.chaoxing.com/",name:"超星"},
+    {url:"http://ncre.neea.edu.cn/",name:"考试网"},
+    {url:"https://www.zhihuishu.com/",name:"智慧树"},
+    {url:"https://im.qq.com/",name:"QQ"},
+]
+
+//常用软件
+let software = [
+    {url:"https://jd.com/",name:"京东"},
+    {url:"https://www.taobao.com/",name:"淘宝"},
+    {url:"https://www.pinduoduo.com/",name:"拼多多"},
+    {url:"https://weibo.com/",name:"微博"},
+    {url:"https://www.zhihu.com/",name:"知乎"},
+	{url:"https://mail.qq.com/",name:"QQ邮箱"},
+    {url:"https://email.163.com/",name:"网易邮箱"},
+    {url:"https://www.12306.cn/",name:"12306"},
+    {url:"https://youdao.com/",name:"有道翻译"},
+    {url:"https://map.baidu.com/",name:"百度地图"},
+    {url:"https://www.amap.com/",name:"高德地图"},
+    {url:"https://mp.weixin.qq.com/",name:"公众平台"},
+    {url:"https://www.jianguoyun.com/",name:"坚果云"},
+    {url:"http://people.cn/",name:"人民网"},
+    {url:"https://www.gmw.cn/",name:"光明网"},
+    {url:"https://meituan.com/",name:"美团"},
+]
+
+
+//资源网站
+let resources = [
+    {url:"https://www.niceso.fun/",name:"奈斯搜索"},
+    {url:"https://www.wuyasou.com/",name:"乌鸦搜"},
+    {url:"https://www.iconfont.cn/",name:"矢量图库"},
+    {url:"https://bz.zzzmh.cn/",name:"极简壁纸"},
+    {url:"https://www.cnki.net/",name:"中国知网"},
+    {url:"https://wall.alphacoders.com/",name:"壁纸"},
+    {url:"https://www.fly63.com/php/ico/",name:"ICO图标"},
+    {url:"https://ico.z01.com/",name:"逐浪图库"},
+    {url:"https://www.crx4chrome.com/",name:"插件"},
+]
+
+//视频与音频网站
+let videoAndaudio = [
+    {url:"https://v.qq.com/",name:"腾讯视频"},
+    {url:"https://iqiyi.com/",name:"爱奇艺"},
+    {url:"https://www.wasu.cn/",name:"华数TV"},
+    {url:"https://youku.com",name:"优酷视频"},
+    {url:"http://www.le.com/",name:"乐视视频"},
+    {url:"https://www.mgtv.com/",name:"芒果TV"},
+    {url:"https://film.sohu.com/",name:"搜狐视频"},
+    {url:"https://www.kugou.com/",name:"酷狗音乐"},
+    {url:"https://y.qq.com/",name:"QQ音乐"},
+    {url:"https://music.163.com/",name:"网易云"},
+    {url:"http://www.kuwo.cn/",name:"酷我音乐"},
+    {url:"https://ximalaya.com/",name:"喜马拉雅"},
+
+]
+
+//常用工具
+let tool = [
+    {url:"https://www.ilovepdf.com/zh-cn",name:"ilove PDF"},
+    {url:"https://convertio.co/zh/",name:"Convertio"},
+    {url:"http://www.ipaddress.com/",name:"IP查询"},
+    {url:"https://www.bootcdn.cn/",name:"bootCDN"},
+    {url:"https://www.onlinedown.net/",name:"华军软件"},
+    {url:"http://pngimg.com/",name:"pngimg"},
+    {url:"https://www.fly63.com/",name:"fly63"},
+    {url:"https://fabiaoqing.com/",name:"发表情"},
+    {url:"https://www.ip138.com/",name:"IP查询"},
+    {url:"https://pixabay.com/",name:"pixabay"},
+    {url:"https://www.pexels.com/",name:"pexels"},
+    {url:"https://images.nasa.gov/",name:"NASA"},
+    {url:"https://www.uupoop.com/",name:"在线PS"},
+    {url:"https://www.aoqiv.com/",name:"aoqiv"},
+    {url:"https://www.kapwing.com/",name:"kapwing"},
+    {url:"http://www.uugai.com/",name:"Logo设计"},
+    {url:"https://www.qt86.com/",name:"艺术字体"},
+    {url:"https://www.designcap.com/cn/",name:"海报设计"},
+    {url:"https://bigjpg.com/",name:"图片放大"},
+    {url:"https://mh.cli.im/",name:"二维码"},
+    {url:"https://carbon.now.sh/",name:"代码截图"},
+    {url:"https://www.remove.bg/zh/",name:"在线抠图"},
+    {url:"https://www.fly63.com/tool/svg2img/",name:"SVG到PNG"},
+    {url:"https://www.fly63.com/tool/giftxt/",name:"视频到GIF"},
+    {url:"https://www.fly63.com/tool/ewm/",name:"二维码"},
+    {url:"https://www.fly63.com/php/decoder/",name:"解码"},
+    {url:"https://www.eteste.com/",name:"字数统计"},
+    {url:"https://picx.xpoet.cn/",name:"图床配置"},
+    {url:"https://sm.ms/login",name:"图床"},
+    {url:"https://imgkr.com/",name:"图壳"},
+    {url:"https://urlify.cn/",name:"短链接"},
+    {url:"https://www.fly63.com/tool/textreplace/",name:"文本替换"},
+    {url:"https://tools.pdf24.org/zh/",name:"PDF工具"},
+    {url:"https://smallpdf.com/cn/",name:"PDF工具"},
+    {url:"https://cn.office-converter.com/",name:"类型转换"},
+    {url:"https://docsmall.com/",name:"文件压缩"},
+]
+
+//书籍搜索
+let book = [
+    {url:"https://zh.ng1lib.org/",name:"ZLibrary"},
+    {url:"https://ebook2.lorefree.com/",name:"lorefree"},
+    {url:"https://www.bandubook.com/",name:"伴读"},
+    {url:"http://www.jb51.net/",name:"脚本之家"},
+    {url:"http://www.xz577.com",name:"码农之家"},
+    {url:"http://www.toplinks.cc/",name:"淘链客"},
+    {url:"https://www.kindle8.cc/",name:"kindle 吧"},
+]
+
+//计算机类
+let computer = [
+    {url:"https://github.com/",name:"GitHub"},
+    {url:"https://leetcode-cn.com/",name:"LeetCode"},
+    {url:"https://dasai.lanqiao.cn/",name:"蓝桥杯"},
+    {url:"https://gitee.com/",name:"码云"},
+    {url:"http://getbootstrap.com/",name:"bootstrap"},
+    {url:"http://jquery.com",name:"jQuery"},
+]
+
+//其他
+let other = [
+    {url:"https://www.qq.com/",name:"腾讯网"},
+    {url:"https://www.alibabagroup.com/",name:"阿里巴巴"},
+    {url:"https://huawei.com/",name:"华为"},
+    {url:"https://oppo.com/",name:"OPPO"},
+    {url:"https://vivo.com/",name:"VIVO"},
+    {url:"https://mi.com/",name:"小米"},
+    {url:"https://www.huaweicloud.com/",name:"华为云"},
+    {url:"https://www.aliyun.com/",name:"阿里云"},
+    {url:"https://cloud.tencent.com/",name:"腾讯云"},
+];
 
